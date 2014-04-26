ef0e71293bbda5dcbc9777b1dd72385bb73d8581
ianiD <123ioandragomir123@gmail.com>
2014-04-26 16:11:27 +0300

    update

--- main.cpp
+++ main.cpp
@@ -5,15 +5,27 @@
 FILE* logs;
 
 bool running;
+bool justEnteredMMenu;
 
 #define logoMillis 2000
+
+#define walkTime 100
+#define standTime 500
 
 #define NONE 0
 #define LOGOS 1
 #define MAIN_MENU 2
-#define GAME 3
+#define NEW_GAME_SCREEN 3
+#define STORY 4
+#define INSTRUCTIONS 5
+#define GAME 10
 
 int currentGameState = NONE;
+
+void gotoMainMenu(){
+    currentGameState = MAIN_MENU;
+    justEnteredMMenu=true;
+}
 
 SDL_Window* gWin = NULL;
 SDL_Surface* gWinSrf = NULL;
@@ -27,9 +39,13 @@ SDL_Surface* ioanD_logo = NULL;
 
 SDL_Surface* winIcon = NULL;
 
+SDL_Surface* gameTitle = NULL;
+
 SDL_Surface* playButton = NULL, *quitButton = NULL;
 
 SDL_Surface* alpha1 = NULL;
+
+SDL_Surface* playerMole = NULL;
 
 #define WIDTH 640
 #define HEIGHT 480
@@ -37,12 +53,33 @@ SDL_Surface* alpha1 = NULL;
 int init_sdl();
 int loadMedia();
 void gameLoop();
-void cleanUp();
+void cleanUp() {
+    running = false;
+    SDL_Quit();
+    IMG_Quit();
+
+    SDL_DestroyWindow(gWin);
+    SDL_FreeSurface(gWinSrf);
+}
+
+bool keys[512];
+
+struct button {
+    SDL_Rect b;
+    SDL_Surface* s;
+    void (*trigger)();
+    int gameState;
+} buttons[256];
+int buttoncount;
 
 SDL_Surface* load(char* url){
     SDL_Surface* result=NULL;
     result = IMG_Load(url);
     return result;
+}
+
+void quit() {
+    running = false;
 }
 
 int main(int argc, char* argv[]){
@@ -58,7 +95,7 @@ int init_sdl() {
     SDL_Init(SDL_INIT_EVERYTHING);
     IMG_Init(IMG_INIT_PNG|IMG_INIT_JPG);
 
-    gWin = SDL_CreateWindow("Ludum dare 29",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WIDTH,HEIGHT,SDL_WINDOW_SHOWN);
+    gWin = SDL_CreateWindow("Extinct",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WIDTH,HEIGHT,SDL_WINDOW_SHOWN);
     if(gWin == NULL){
         fprintf(logs,"could not create window!!!");
         return 1;
@@ -69,6 +106,11 @@ int init_sdl() {
         fprintf(logs,"Could not get the window's surface!!!");
         return 1;
     }
+}
+
+void nothing(){}
+void play() {
+    currentGameState = NEW_GAME_SCREEN;
 }
 
 int loadMedia(){
@@ -84,6 +126,12 @@ int loadMedia(){
     if((winIcon     = load("res/icon.png"))==NULL)
         return 1;
 
+    if((playerMole  = load("res/sprites/MolePlayer.png"))==NULL)
+        return 1;
+
+    if((gameTitle   = load("res/logo/gameTitle.png"))==NULL)
+        return 1;
+
     if((playButton  = load("res/buttons/playButton.png"))==NULL)
         return 1;
     if((quitButton  = load("res/buttons/quitButton.png"))==NULL)
@@ -93,9 +141,70 @@ int loadMedia(){
         return 1;
 }
 
+bool clickedButton(button b, int x, int y){
+    bool wx = b.b.x<=x&&(b.b.x+b.b.w)>=x;
+    bool wy = b.b.y<=y&&(b.b.y+b.b.h)>=y;
+    return wx&&wy;
+}
+
+void handleEvent(SDL_Event e){
+    switch(e.type){
+        case SDL_QUIT: {
+            cleanUp();
+            break;
+        }
+        case SDL_KEYDOWN: {
+            keys[e.key.keysym.scancode]=true;
+            break;
+        }
+        case SDL_KEYUP: {
+            keys[e.key.keysym.scancode]=false;
+            break;
+        }
+        case SDL_MOUSEBUTTONDOWN: {
+            int mx = e.button.x, my = e.button.y;
+            for(int i=0;i<buttoncount;i++){
+                if(clickedButton(buttons[i],mx,my))
+                    buttons[i].trigger();
+            }
+        }
+    }
+}
+
+void makeButton(SDL_Surface* sprite, SDL_Rect rect, void (*trig)(), int gameState){
+    buttons[buttoncount].s=sprite;
+    buttons[buttoncount].b=rect;
+    buttons[buttoncount].trigger = *trig;
+    buttons[buttoncount].gameState = gameState;
+    buttoncount++;
+}
+
+void tick(){
+    SDL_Event e;
+    while(SDL_PollEvent(&e))
+        handleEvent(e);
+
+}
+
+void render() {
+    for(int i=0;i<buttoncount;i++){
+        SDL_BlitScaled(buttons[i].s,NULL,gWinSrf,&buttons[i].b);
+    }
+    SDL_UpdateWindowSurface(gWin);
+    SDL_FillRect(gWinSrf,&gWinSrf->clip_rect,0);
+}
+
 void gameLoop() {
+    SDL_SetWindowIcon(gWin,winIcon);
+    int ticks = 0; //-> should allways be 60 (sometimes 59 or 61)
+    int frames = 0;
+    long long timer = SDL_GetTicks();
+    long long lastTime = SDL_GetTicks();
+    const double ms = 1000.0/60.0;
+    double delta = 0;
     running = true;
     while(running){
+        long long now = SDL_GetTicks();
         switch(currentGameState){
             case NONE: {
                 currentGameState = LOGOS;
@@ -111,7 +220,7 @@ void gameLoop() {
                 while(SDL_GetTicks()-timer<=logoMillis&&!keypressed){
                     SDL_Event e;
                     while(SDL_PollEvent(&e))
-                        if(e.type==SDL_KEYDOWN)
+                        if(e.type==SDL_KEYDOWN||e.type==SDL_QUIT)
                             keypressed=true;
                     SDL_UpdateWindowSurface(gWin);
                 }
@@ -132,7 +241,7 @@ void gameLoop() {
                 while(SDL_GetTicks()-timer<=logoMillis&&!keypressed){
                     SDL_Event e;
                     while(SDL_PollEvent(&e))
-                        if(e.type==SDL_KEYDOWN)
+                        if(e.type==SDL_KEYDOWN||e.type==SDL_QUIT)
                             keypressed=true;
                     SDL_UpdateWindowSurface(gWin);
                 }
@@ -154,7 +263,7 @@ void gameLoop() {
                 while(SDL_GetTicks()-timer<=logoMillis&&!keypressed){
                     SDL_Event e;
                     while(SDL_PollEvent(&e))
-                        if(e.type==SDL_KEYDOWN)
+                        if(e.type==SDL_KEYDOWN||e.type==SDL_QUIT)
                             keypressed=true;
                     SDL_UpdateWindowSurface(gWin);
                 }
@@ -167,8 +276,67 @@ void gameLoop() {
                 SDL_FillRect(gWinSrf,&gWinSrf->clip_rect,0);
                 SDL_UpdateWindowSurface(gWin);
 
-                currentGameState = MAIN_MENU;
+                gotoMainMenu();
+            }
+            case MAIN_MENU: {
+                SDL_FillRect(gWinSrf,&gWinSrf->clip_rect,0xffd5ad);
+                if(justEnteredMMenu){
+                    justEnteredMMenu=false;
+                    SDL_Rect buttonRect;
+                    buttonRect.x=WIDTH/2-128;
+                    buttonRect.y=10;
+                    buttonRect.h=64;
+                    buttonRect.w=256;
+                    makeButton(gameTitle,buttonRect,&nothing, MAIN_MENU);
+
+                    buttonRect.x=WIDTH/2-64;
+                    buttonRect.y=80;
+                    buttonRect.w=128;
+                    makeButton(playButton,buttonRect,&play, MAIN_MENU);
+
+                    buttonRect.x=WIDTH/2-64;
+                    buttonRect.y=150;
+                    buttonRect.w=128;
+                    makeButton(quitButton,buttonRect,&quit, MAIN_MENU);
+                }
+                break;
+            }
+            case GAME: {
+                int standingframe = SDL_GetTicks() / standTime % 2;
+                int walkingframe = SDL_GetTicks() / walkTime % 4;
+                SDL_Rect standrect, standrectd, walkrect, walkrectd;
+                standrect.x=standingframe*64;
+                standrect.y=0;
+                standrect.h=128;
+                standrect.w=64;
+                standrectd.x=standrectd.y=0;
+                standrectd.w=64;
+                standrectd.h=128;
+                SDL_BlitSurface(playerMole,&standrect,gWinSrf,&standrectd);
+                walkrect.x=walkingframe*64;
+                walkrect.y=0;
+                walkrect.h=128;
+                walkrect.w=64;
+                walkrectd.x=100;
+                walkrectd.y=0;
+                walkrectd.w=64;
+                walkrectd.h=128;
+                SDL_BlitSurface(playerMole,&walkrect,gWinSrf,&walkrectd);
+                break;
             }
         }
-    }
+            delta += (now-lastTime)/ms;
+            lastTime = SDL_GetTicks();
+            while(delta>=1.0){
+                tick();
+                ticks++;
+                delta = delta - 1.0;
+            }
+            render();
+            frames++;
+            if(SDL_GetTicks()-timer>1000){
+                ticks = frames = 0;
+                timer += 1000;
+            }
+        }
 }
